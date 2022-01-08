fff5e4025014f8d51a358dd1be1615350546c09e
Md. Shahadat Hossain <mhossain173032@bscse.uiu.ac.bd>
2022-01-08 13:15:12 +0600

    Singleton Pattern implemented

--- MyFancyJavaProject .java
+++ MyFancyJavaProject .java
@@ -1,19 +1,23 @@
-    public class singleton{  
+public class singleton{  
         static String c;
+        private static singleton obj=null;
         private singleton(String a,String b)
         {
           this.c=a.concat(b);  
         }  
        
-     public static singleton get_singleton(singleton obj){  
-        return obj;  
+        public static singleton get_singleton(String a, String b){  
+	        if (obj==null)
+                obj=new singleton(a,b);
+	
+            return obj;  
      }  
-      
-     public static void main(String[] args) throws Exception{  
-        String a="Instance";
-	    String b=" Created";
-	    singleton create_obj=new singleton(a,b);
-        singleton store_obj=singleton.get_singleton(create_obj);
-        System.out.println(store_obj.c);
-     }  
-    }  
+     	 public static void main(String[] args) throws Exception
+	 {
+	     String a="Instance";
+	     String b=" Created & modified to singleton";
+             singleton store_obj=singleton.get_singleton(a,b);
+             System.out.println(store_obj.c);
+	 }
+  
+    }
