d33c5d20f2a918b57824fac5cbd82d27f342b34d
Fahim-Al-Awsaf <fawsaf181253@bscse.uiu.ac.bd>
2022-01-09 18:22:14 +0600

    commited changes on 9th January

--- MyFancyJavaProject .java
+++ MyFancyJavaProject .java
@@ -1,8 +1,26 @@
     public class singleton{  
+        public static int count_func(String n) { 
+            int s = 0; 
+            int len=n.length()+1;
+            char[] ch=new char[len];
+      
+            for (int i=0; i<n.length();i++) {
+                ch[i]=n.charAt(i);
+            }
+            for (int i=0;ch[i]!='\0';i++)
+            {
+                if(ch[i]==' ')
+                    s++;
+            }
+            return s+1; 
+        }
+        
         static String c;
+        static int count;
         private singleton(String a,String b)
         {
           this.c=a.concat(b);  
+          this.count=count_func(c);
         }  
        
      public static singleton get_singleton(singleton obj){  
@@ -10,10 +28,11 @@
      }  
       
      public static void main(String[] args) throws Exception{  
-        String a="Instance";
-	    String b=" Created";
+        String a="Count word";
+    	String b=" containing in the final string";
 	    singleton create_obj=new singleton(a,b);
         singleton store_obj=singleton.get_singleton(create_obj);
         System.out.println(store_obj.c);
+        System.out.println("Word count of string: " + store_obj.count);
      }  
     }  
